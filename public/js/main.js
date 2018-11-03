aace0f10e24f65a94a9abf87ea3f3267114d9de3
Ryan Jacoby <rjacoby00@icloud.com>
2018-11-02 23:09:17 -0700

    Fix earlier commit

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,17 @@
 var socket = io();
 
 socket.on('chat message', function(msg){
-  var elm = document.createElement('li');
-  console.log("Content" +  msg);
-  elm.innerHTML = msg;
+  parsed = JSON.parse(msg);
+  var elm = document.createElement('tr');
+  elm.innerHTML = "<td>"+parsed.User+"</td><td>"+parsed.Message+"</td>";
   document.getElementById('messages').appendChild(elm);
 });
 
 document.getElementById('submit').addEventListener('click', function(){
-  socket.emit('chat message', document.getElementById('message').value);
+  message = document.getElementById('message').value;
+  user = document.getElementById('user').value;
+  socket.emit('chat message', '{ "User":"'+user+'","Message":"'+message+'"}');
+  document.getElementById('message').value = "";
 });
 
 document.getElementById('message').addEventListener("keydown", function(event) {

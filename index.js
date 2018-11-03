cad66e78631e20a580795405183f786c410a5b1f
Ryan Jacoby <rjacoby00@icloud.com>
2018-11-02 22:17:54 -0700

    Fix more heroku deployment issues

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 80
+const port = process.env.PORT || 5000
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 

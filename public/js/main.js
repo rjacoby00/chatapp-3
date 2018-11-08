b9608a08c1c1e69c83467bb2c52555658ff8e303
Nathan McCutchen <n8wm@icloud.com>
2018-11-08 12:26:06 -0800

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -3,12 +3,11 @@ var socket = io();
 socket.on('chat message', function(msg){
   parsed = JSON.parse(msg);
   var elm = document.createElement('div');
+  elm.classList.add("bblmsg");
+  elm.innerHTML = "<span class='bblmsg-name'>"+parsed.User+"</span><span class='bblmsg-msg'>"+parsed.Message+"</span>";
   if (parsed.User == document.getElementById('user').value) {
     elm.classList.add("bblmsg-self");
     elm.innerHTML = "<span class='bblmsg-msg'>"+parsed.Message+"</span>";
-  } else {
-    elm.classList.add("bblmsg");
-    elm.innerHTML = "<span class='bblmsg-name'>"+parsed.User+"</span><span class='bblmsg-msg'>"+parsed.Message+"</span>";
   }
   document.getElementById('messages').appendChild(elm);
 });

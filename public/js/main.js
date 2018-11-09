144d0bc92e982f8833e9676fe9c4d364cf357432
Ryan Jacoby <rjacoby00@icloud.com>
2018-11-08 18:52:05 -0800

    multiple messages from the same person, people cant send empty messages

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,38 @@
 var socket = io();
+var lastUser = '';
 
 socket.on('chat message', function(msg){
   parsed = JSON.parse(msg);
   var elm = document.createElement('div');
   elm.classList.add("bblmsg");
   if (parsed.User == document.getElementById('user').value) {
+    if (lastUser == parsed.User) {
+      last_messsge = document.getElementsByClassName('bblmsg-self')[document.getElementsByClassName('bblmsg-self').length-1];
+      last_messsge.classList.remove("bblmsg-self");
+      last_messsge.classList.add("bblmsg-self-repeat");
+    }
     elm.innerHTML = "<span class='bblmsg-msg bblmsg-self'>"+parsed.Message+"</span>";
   } else {
-    elm.innerHTML = "<span class='bblmsg-name'>"+parsed.User+"</span><span class='bblmsg-msg bblmsg-other'>"+parsed.Message+"</span>";
+    if (lastUser == parsed.User) {
+      last_messsge = document.getElementsByClassName('bblmsg-other')[document.getElementsByClassName('bblmsg-self').length-1];
+      last_messsge.classList.remove("bblmsg-other");
+      last_messsge.classList.add("bblmsg-other-repeat");
+      elm.innerHTML = "</span><span class='bblmsg-msg bblmsg-other'>"+parsed.Message+"</span>";
+    } else {
+      elm.innerHTML = "<span class='bblmsg-name'>"+parsed.User+"</span><span class='bblmsg-msg bblmsg-other'>"+parsed.Message+"</span>";
+    }
   }
   document.getElementById('messages').appendChild(elm);
+  lastUser =  parsed.User;
 });
 
 document.getElementById('submit').addEventListener('click', function(){
   message = document.getElementById('message').value;
-  user = document.getElementById('user').value;
-  socket.emit('chat message', '{ "User":"'+user+'","Message":"'+message+'"}');
-  document.getElementById('message').value = "";
+  if (message) {
+    user = document.getElementById('user').value;
+    socket.emit('chat message', '{ "User":"'+user+'","Message":"'+message+'"}');
+    document.getElementById('message').value = "";
+  }
 });
 
 document.getElementById('message').addEventListener("keydown", function(event) {

89517c95933dd4316f2663eb3b95837201a732af
Nathan McCutchen <n8wm@icloud.com>
2019-03-31 19:31:12 -0700

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-    socket.emit('chat message', msg);
+    io.emit('chat message', msg);
   });
 });
 
